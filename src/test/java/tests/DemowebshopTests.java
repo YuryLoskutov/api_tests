f97ed2710f34b9236e5de32663d1c1d0c236d01f
YuryLoskutov <megafudziyama@gmail.com>
2022-06-22 23:06:22 +0300

    Added stuff from lesson 18

--- src/test/java/tests/DemowebshopTests.java
+++ src/test/java/tests/DemowebshopTests.java
@@ -3,14 +3,16 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.Cookie;
 
-import java.util.Map;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static helpers.CustomApiListener.withCustomTemplates;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 
@@ -18,10 +20,12 @@ public class DemowebshopTests {
 
     static String login = "qaguru@qa.guru",
             password = "qaguru@qa.guru1",
-            authorizationCookieName = "NOPCOMMERCE.AUTH";
+            authCookieName = "NOPCOMMERCE.AUTH";
 
     @BeforeAll
-    static void configureBaseUrl() {
+    static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.baseUrl = "http://demowebshop.tricentis.com";
         RestAssured.baseURI = "http://demowebshop.tricentis.com";
     }
@@ -54,23 +58,97 @@ public class DemowebshopTests {
     @DisplayName("Successful authorization to some demowebshop (UI)")
     void loginWithApiTest() {
         step("Get cookie and set it into browser", () -> {
-        String authorizationCookiesValue = given()
-                .contentType("application/x-www-form-urlencoded")
-                .formParam("Email", login)
-                .formParam("Password", password)
-                .log().all()
-                .when()
-                .post("/login")
-                .then()
-                .log().all()
-                .statusCode(302)
-                .extract().cookie(authorizationCookieName);
+                    String authCookiesValue = given()
+                            .contentType("application/x-www-form-urlencoded")
+                            .formParam("Email", login)
+                            .formParam("Password", password)
+                            .log().all()
+                            .when()
+                            .post("/login")
+                            .then()
+                            .log().all()
+                            .statusCode(302)
+                            .extract().cookie(authCookieName);
 
-        open("/Themes/DefaultClean/Content/images/logo.png");
-        Cookie ck = new Cookie(authorizationCookieName, authorizationCookiesValue);
-        WebDriverRunner.getWebDriver().manage().addCookie(ck);
 
-        open("/");
+                    step("Open minimal content", () ->
+                            open("/Themes/DefaultClean/Content/images/logo.png"));
+
+                    step("Set cookie to the browser", () -> {
+                        Cookie authCookie = new Cookie(authCookieName, authCookiesValue);
+                        WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+                    });
+                });
+
+        step("Open main page", () ->
+                open("/"));
+
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization to some demowebshop (UI) with filter")
+    void loginWithApiAndAllureListenerTest() {
+        step("Get cookie and set it into browser", () -> {
+                    String authCookiesValue = given()
+                            //Добавляет логирование в Allure отчёт
+                            .filter(new AllureRestAssured())
+                            .contentType("application/x-www-form-urlencoded")
+                            .formParam("Email", login)
+                            .formParam("Password", password)
+                            .log().all()
+                            .when()
+                            .post("/login")
+                            .then()
+                            .log().all()
+                            .statusCode(302)
+                            .extract().cookie(authCookieName);
+                    step("Open minimal content", () ->
+                            open("/Themes/DefaultClean/Content/images/logo.png"));
+                    step("Set cookie to the browser", () -> {
+                        Cookie authCookie = new Cookie(authCookieName, authCookiesValue);
+                        WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+                    });
+                });
+
+        step("Open main page", () ->
+                open("/"));
+
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization to some demowebshop (UI) with filter")
+    void loginWithApiAndCustomListenerTest() {
+        step("Get cookie and set it into browser", () -> {
+                    String authCookiesValue = given()
+                            //Добавляет катсомное логирование в отчёт, метод withCustomTemplates
+                            .filter(withCustomTemplates())
+                            .contentType("application/x-www-form-urlencoded")
+                            .formParam("Email", login)
+                            .formParam("Password", password)
+                            .log().all()
+                            .when()
+                            .post("/login")
+                            .then()
+                            .log().all()
+                            .statusCode(302)
+                            .extract().cookie(authCookieName);
+                    step("Open minimal content", () ->
+                            open("/Themes/DefaultClean/Content/images/logo.png"));
+                    step("Set cookie to the browser", () -> {
+                        Cookie authCookie = new Cookie(authCookieName, authCookiesValue);
+                        WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+                    });
+                });
+
+        step("Open main page", () ->
+                open("/"));
+
         step("Verify successful authorization", () ->
                 $(".account").shouldHave(text(login)));
     }
